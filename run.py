d8eba1b9c1377b001171914f67cdceb146c4b337
ScottQuinn144 <scott.quinn144@outlook.com>
2022-03-12 14:11:08 +0000

    add print() to beginning of program

--- run.py
+++ run.py
@@ -13,12 +13,19 @@ def play_again():
     '''
     Start a new game or exit the program
     '''
-    another_game = input("Wanna play again? <y>es or <n>o? >: ").lower()
+    another_game = input("Wanna play again? <y>es or <n>o?: ").lower()
     if another_game == "y":
         play_game()
     else:
         print("Goodbye!")
         sys.exit()
+
+
+print("Welcome to Battleships!")
+print("""\nYou have 15 missles and there are 3 ships on the map.
+In order to hit them, you have to enter specific numbers for that location.
+Example: For the first row and first column, you have to write 1 and 1.
+\nGOODLUCK!\n""")
 
 
 def play_game():

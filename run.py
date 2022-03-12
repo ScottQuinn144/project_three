45021a1daa3f683c376a5efd192aac303d37eab8
ScottQuinn144 <scott.quinn144@outlook.com>
2022-03-12 14:01:53 +0000

    play_again and create_random_ship funtions complete

--- run.py
+++ run.py
@@ -6,19 +6,27 @@ def create_random_ship():
     '''
     Creates a random position within the board to place the target
     '''
-    print('1')
+    return random.randint(0, 5), random.randint(0, 5)
+
 
 def play_again():
     '''
     Start a new game or exit the program
     '''
-    print('2')
+    another_game = input("Wanna play again? <y>es or <n>o? >: ").lower()
+    if another_game == "y":
+        play_game()
+    else:
+        print("Goodbye!")
+        sys.exit()
+
 
 def play_game():
     '''
     Guesses and checks with data validation
     '''
     print('3')
+
 
 create_random_ship()
 play_again()

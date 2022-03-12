6b438c1149647093a1900b6645d04cac81b68ce3
ScottQuinn144 <scott.quinn144@outlook.com>
2022-03-12 13:55:39 +0000

    run.py setup complete

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import sys
+
+
+def create_random_ship():
+    '''
+    Creates a random position within the board to place the target
+    '''
+    print('1')
+
+def play_again():
+    '''
+    Start a new game or exit the program
+    '''
+    print('2')
+
+def play_game():
+    '''
+    Guesses and checks with data validation
+    '''
+    print('3')
+
+create_random_ship()
+play_again()
+play_game()
